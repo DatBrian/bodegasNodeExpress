4926f95c9420c10b41cdf96771441433461e199e
DatBrian <vikard.2005@hotmail.com>
2023-07-11 23:42:15 -0500

    refactor: :coffin: Se quitaron comentarios y codigos innecesarios
    Se quitaron los comentarios innecesarios del codigo y se mejoró el archivo env-example para mejor visibilidad

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
 import App from './app';
 import { bodegasRoutes } from './routes/BodegasRoutes';
 import { productosRoutes } from './routes/ProductosRoutes';
-// import env from './config/EnvConfig';
 
 const app = new App([
     bodegasRoutes,
     productosRoutes
 ]);
-
-// app.use(express.json());
 
 app.listen();
